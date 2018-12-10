3a1bd46933f5685460045fa7ada802a940b24d5b
copec <copec@192.168.15.4>
2018-12-09 22:30:25 -0600

    arreglado todo, no guarda usuarios aún.

--- src/Managers/ManejadorUsuarios.java
+++ src/Managers/ManejadorUsuarios.java
@@ -64,12 +64,4 @@ public class ManejadorUsuarios {
         manejadorArchivo.cerrarArchivo();
         return usuarios;
     }
-    public void jajatl(){
-        if(usuarios.isEmpty()){
-            System.out.println("no hay nada");
-        }
-        for(Usuarios.Usuario user : usuarios){
-            System.out.println(user.getNombre()+ " " + user.getPass());
-        }
-    }
 }
